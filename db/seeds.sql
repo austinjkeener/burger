77aea2edbe9c15b5f9234ab9cb51461d4beb6633
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-13 09:37:26 -0400

    sudo coding

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burger_db;
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("veggie-burger", true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("veggie-burger low cal", true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("veggie-burger extra veggi", true);
